b79af508da043bda301bd8f19e28844b253c3cc6
Mohit <mohiitsiingh10@gmail.com>
2026-01-21 16:16:49 +0000

    Jan 21, 2026, 9:46 PM

---
+++ ques5.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+int main(){
+/*Q5. A student wants to swap the values of two variables for practising coding basics. 
+Implement a solution to swap two numbers using different techniques. */
+int a =10,b=2,c;
+c = a;
+a = b;
+b= c;
+std::cout<<"swap a and b: a is: "<<a<<"\nb is: "<<b;
+    return 0;
+}
