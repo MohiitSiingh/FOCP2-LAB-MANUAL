b7f8dea31a862a6f20fbc3529b130ab8744e7fa4
Mohit <mohiitsiingh10@gmail.com>
2026-02-02 04:26:51 +0000

    Feb 2, 2026, 9:56 AM

--- ques29.cpp
+++ ques29.cpp
@@ -1,7 +1,20 @@
 #include<iostream>
-int main()[
+int main(){
 /*Q29. A weather monitoring app records 30-day temperature logs.
  Implement a solution to store daily 
 temperatures in an array and display the minimum temperature for the month.*/
+std::cout<<"Enter temperature: ";
+float temp[30];
+float min = temp[0] ;
+for(int i=0; i<30 ; i++){
+    std::cin>>temp[i];
+    
+}
+for(int i=0; i<30;i++){
+if(temp[i+1]<temp[0]){
+    min = temp[i+1];
+    }
+}
+std::cout<<"minimum temperatre recorded is: "<<min;
     return 0;
-]
+ }
