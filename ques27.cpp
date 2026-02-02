08839c3538c886d89d9c8d4c73162fe1ab58d39a
Mohit <mohiitsiingh10@gmail.com>
2026-02-02 04:03:09 +0000

    Feb 2, 2026, 9:33 AM

--- ques27.cpp
+++ ques27.cpp
@@ -4,9 +4,9 @@ int main(){
  store the prices of 10
 items in an array and display the maximum price*/
 float price[10];
-std::cout<<"enter price: "
+std::cout<<"enter price: ";
 float max=price[0];
-for(int = 0 ; i<10 ; i++){
+for(int i = 0 ; i<10 ; i++){
 std::cin>>price[i];
 }
 for(int i = 0; i<10;i++){
