7c0347defa01ced17c8226f3b15599845a916a8f
Mohit <mohiitsiingh10@gmail.com>
2026-02-02 04:57:03 +0000

    Feb 2, 2026, 10:27 AM

--- ques29.cpp
+++ ques29.cpp
@@ -4,12 +4,14 @@ int main(){
  Implement a solution to store daily 
 temperatures in an array and display the minimum temperature for the month.*/
 std::cout<<"Enter temperature: ";
-int temp[30];
+
 int min = temp[0] ;
 for(int i=0; i<30 ; i++){
     std::cin>>temp[i];
     
 }
+int min = temp[0];
+
 for(int i=1; i<30;i++){
 if(temp[i]<min){
     min = temp[i];
