a7331d9f498aab7a6022d1c7a1fe9b8f71638405
Mohit <mohiitsiingh10@gmail.com>
2026-02-02 04:14:24 +0000

    Feb 2, 2026, 9:44 AM

--- ques28.cpp
+++ ques28.cpp
@@ -3,6 +3,18 @@ int main(){
 /*Q28. A data processing system classifies even and odd inputs separately. 
 Implement a solution to
 store 5 elements in an array, compute sum of all even and sum of all odd numbers.*/
-std::cout"enter "
+std::cout<<"enter number:  ";
+int oddtotal=0,eventotal=0;
+int num[5];
+for(int i =0; i<5 ; i++){
+std::cin>>num[i];
+if(num[i]/2==0){
+    eventotal= eventotal+num[i];
+}
+else if (num[i]/2!=0){
+    oddtotal+=num[i];
+}
+}
+std::cout<<"sum of odd:"<<oddtotal<<"sum of even: "<<eventotal;
     return 0 ;
 }
