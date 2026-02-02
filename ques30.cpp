61d836de6c644776dbc7bdff294a84cfa224dc94
Mohit <mohiitsiingh10@gmail.com>
2026-02-02 05:02:47 +0000

    Feb 2, 2026, 10:32 AM

--- ques30.cpp
+++ ques30.cpp
@@ -4,5 +4,7 @@ int main(){
      Implement a solution to accept salary of
 10 employees in an array, compute total salary and average salary,
  then display the result*/
-int 
+int salary[10];
+std::cout<<"enter salary: ";
+
 }
