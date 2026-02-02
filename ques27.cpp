22c42819bf88c138de6cd1e7727b5770ad5063e5
Mohit <mohiitsiingh10@gmail.com>
2026-02-02 03:51:44 +0000

    Feb 2, 2026, 9:21 AM

---
+++ ques27.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+int main(){
+/*Q27. A supermarket software maintains item price lists. Implement a solution to store the prices of 10
+items in an array and display the maximum price*/
+
+    return 0;
+}
