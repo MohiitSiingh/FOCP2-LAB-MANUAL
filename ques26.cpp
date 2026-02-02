c0bb9429269595382ce94dc7e2313662d5256381
Mohit <mohiitsiingh10@gmail.com>
2026-02-02 03:54:16 +0000

    Feb 2, 2026, 9:24 AM

--- ques26.cpp
+++ ques26.cpp
@@ -4,9 +4,11 @@ int main(){
 Implement a solution to accept marks in 5 subjects,
  compute the total and percentage, and display the result*/
  float sub[5];
+ float total = 0;
  std::cout<<"Enter marks for subject: ";
  for(int i =0 ; i<5 ; i++){
     std::cin>>sub[i];
+    total = total + sub[i];
  }
  float total = sub[0] + sub[1] + sub[2] + sub[3] + sub[4];
  float percent = (total/500)*100;
--- ques27.cpp
+++ ques27.cpp
@@ -1,7 +1,13 @@
 #include<iostream>
 int main(){
-/*Q27. A supermarket software maintains item price lists. Implement a solution to store the prices of 10
+/*Q27. A supermarket software maintains item price lists. Implement a solution to
+ store the prices of 10
 items in an array and display the maximum price*/
+float price[10];
+std
+for(int = 0 ; i<10 ; i++){
+
+}
 
     return 0;
 }
