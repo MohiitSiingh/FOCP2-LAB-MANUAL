7129ba341b8935b1e42f95967225937e238233e0
Mohit <mohiitsiingh10@gmail.com>
2026-02-02 04:08:07 +0000

    Feb 2, 2026, 9:38 AM

---
+++ ques28.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+int main(){
+/*Q28. A data processing system classifies even and odd inputs separately. 
+Implement a solution to
+store 5 elements in an array, compute sum of all even and sum of all odd numbers.*/
+std::cout"enter "
+    return 0 ;
+}
