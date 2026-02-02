3f78f609150d1fd69e931ac8fa20945f6cc0ee13
Mohit <mohiitsiingh10@gmail.com>
2026-02-02 03:49:24 +0000

    Feb 2, 2026, 9:19 AM

--- ques26.cpp
+++ ques26.cpp
@@ -4,13 +4,14 @@ int main(){
 Implement a solution to accept marks in 5 subjects,
  compute the total and percentage, and display the result*/
  float sub[5];
+ std::cout<<"Enter marks for subject: ";
  for(int i =0 ; i<5 ; i++){
     std::cin>>sub[i];
  }
  float total = sub[0] + sub[1] + sub[2] + sub[3] + sub[4];
  float percent = (total/500)*100;
  
-    std::cout<<total<<std::endl<<percent;
+    std::cout<<"total marks are"<<total<<std::endl<<"percentage is "<<percent;
  
 
     return 0;
