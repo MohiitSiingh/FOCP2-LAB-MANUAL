8dbc02cbf7de6d092a3b342151d6ebbd4537dbc9
Mohit <mohiitsiingh10@gmail.com>
2026-02-02 03:45:58 +0000

    Feb 2, 2026, 9:15 AM

---
+++ ques26.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+int main(){
+/*Q26. The school report card system stores subject marks for each student.
+Implement a solution to accept marks in 5 subjects,
+ compute the total and percentage, and display the result*/
+ float sub[5];
+ for(int i =0 ; i<5 ; i++){
+    std::cin>>sub[i];
+ }
+ float total = sub[0] + sub[1] + sub[2] + sub[3] + sub[4];
+ float percent = (total/500)*100;
+ for(int i = 0 ; i<5 ; i++){
+    std::cout<<
+ 
+
+    return 0;
+}
