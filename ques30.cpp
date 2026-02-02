256003985e92d9a533f1b278e94a87aa2f984ef8
Mohit <mohiitsiingh10@gmail.com>
2026-02-02 04:45:26 +0000

    Feb 2, 2026, 10:15 AM

--- ques30.cpp
+++ ques30.cpp
@@ -1,4 +1,7 @@
 #include<iostream>
 int main(){
-    /**/
+    /*Q30. A payroll system maintains employee salary records.
+     Implement a solution to accept salary of
+10 employees in an array, compute total salary and average salary,
+ then display the result*/
 }
