79a6e4373d1e852194fa3e30ae06f582b508cdaf
Mohit <mohiitsiingh10@gmail.com>
2026-03-10 07:22:26 +0000

    Mar 10, 2026, 12:52 PM

---
+++ ques7.cpp
@@ -0,0 +1,8 @@
+/* Q7. A game compares three players' scores to find who is ahead.
+ Implement a solution to accept three scores
+and identify the winner. */
+#include<iostream>
+int main(){
+
+    return 0;
+}
