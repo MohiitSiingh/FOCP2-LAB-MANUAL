ecc053e0fa29d8bf284ea726c07cc7dbe2159ce8
Mohit <mohiitsiingh10@gmail.com>
2026-03-10 07:48:59 +0000

    Mar 10, 2026, 1:18 PM

--- ques8.cpp
+++ ques8.cpp
@@ -4,3 +4,6 @@ To make logs easier to analyse, the system applies tags to certain events based
 • Events whose ID is divisible by 3 are tagged as “Buzz”
 • Events whose ID is divisible by 5 are tagged as “Fuzz”
 • Events divisible by both 3 and 5 receive both tags*/
+#include<iostream>
+#include<string>
+using namespace std;
