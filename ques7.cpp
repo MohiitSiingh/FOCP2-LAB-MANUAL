f2425b74dc5ae23504e627e9b265d385a6d037d6
Mohit <mohiitsiingh10@gmail.com>
2026-03-10 07:34:16 +0000

    Mar 10, 2026, 1:04 PM

--- ques7.cpp
+++ ques7.cpp
@@ -2,6 +2,7 @@
  Implement a solution to accept three scores
 and identify the winner. */
 #include<iostream>
+using namespace std;
 int main(){
 
 int players;
@@ -9,9 +10,14 @@ float score[players];
 for(int i=0 ; i<players ;i++){
 cout<<"player "<<i;
 cin>>score[i];
-
 }
-
+for(int i = 0; i<players; i++){  
+float highest = score[0];
+if (score[i]>highest){
+    highest = score[i];
+}
+cout<<"player "<<i<<"has highest score"<<score[i];
+}
 
     return 0;
 }
