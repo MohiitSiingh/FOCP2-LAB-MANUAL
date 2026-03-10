4106914b48bed0ef559d0599987a14b066256df3
Mohit <mohiitsiingh10@gmail.com>
2026-03-10 07:16:02 +0000

    Mar 10, 2026, 12:46 PM

--- ques6.cpp
+++ ques6.cpp
@@ -3,9 +3,3 @@ fixed 12% bonus for each worker.
 Implement a solution to accept the number of employees and 
 their basic salary. Compute bonus, net salary, and
 display results.*/
-#include<iostream>
-#include<string>
-int main(){
-    
-    return 0;
-}
