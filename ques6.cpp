98bff0472eae8a96fad51a10699106e57198e90c
Mohit <mohiitsiingh10@gmail.com>
2026-03-10 07:15:18 +0000

    Mar 10, 2026, 12:45 PM

---
+++ ques6.cpp
@@ -0,0 +1,11 @@
+/*Q6. An HR system needs to calculate employees’ net salaries, including a 
+fixed 12% bonus for each worker.
+Implement a solution to accept the number of employees and 
+their basic salary. Compute bonus, net salary, and
+display results.*/
+#include<iostream>
+#include<string>
+int main(){
+    
+    return 0;
+}
