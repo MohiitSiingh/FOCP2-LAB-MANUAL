cd22856a842f52eb7dbfc021e6b8ce568842303f
Mohit <mohiitsiingh10@gmail.com>
2026-03-10 08:38:02 +0000

    Mar 10, 2026, 2:08 PM

--- ques9.cpp
+++ ques9.cpp
@@ -1,5 +1,8 @@
 /*   Q9. A text editor auto-detects whether an input letter is a vowel, a consonant or a number. Implement a
 solution to classify the symbol */
+#include<iostream>
+#include<string>
 int main(){
     
+    return 0;
 }
